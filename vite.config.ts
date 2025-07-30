d92a3be74b56c0e6891860757d77afd3019d011a
reikemdev <jimmymeneses11@gmail.com>
2025-07-30 15:59:46 -0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/bne_reinvention/",   // ← EXACTO al path que usas en GitHub Pages
+  base: "/bne_reinvention/",  
+build:{outDir:"docs"},
   plugins: [react()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
